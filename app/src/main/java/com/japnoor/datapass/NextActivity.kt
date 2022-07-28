d7fb880829601efedabb3dc7d9631937160b9cd5
Japnoor2104 <sjapnoor11@gmail.com>
2022-07-28 14:42:59 +0530

    change

--- app/src/main/java/com/japnoor/datapass/NextActivity.kt
+++ app/src/main/java/com/japnoor/datapass/NextActivity.kt
@@ -6,30 +6,30 @@ import com.japnoor.datapass.databinding.ActivityNextBinding
 
 class NextActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityNextBinding
+    lateinit var binding: ActivityNextBinding
     var str: String = ""
-    var inte : Int=0
-    var doub : Double=0.0
-    var flo : Float=0.0f
-    var cha : Char='a'
+    var inte: Int = 0
+    var doub: Double = 0.0
+    var flo: Float = 0.0f
+    var cha: Char = 'a'
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityNextBinding.inflate(layoutInflater)
+        binding = ActivityNextBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         intent?.let {
-            if(it.hasExtra("str"))
-            str=it.getStringExtra("str") as String
-            if(it.hasExtra("inte"))
-            inte=it.getIntExtra("inte",0)
-            if(it.hasExtra("doub"))
-            doub=it.getDoubleExtra("doub",0.0)
-            if(it.hasExtra("flo"))
-                flo=it.getFloatExtra("flo",0.0f)
-            if(it.hasExtra("cha"))
-                cha=it.getCharExtra("char",'a')
-        }?:kotlin.run {  }
+            if (it.hasExtra("str"))
+                str = it.getStringExtra("str") as String
+            if (it.hasExtra("inte"))
+                inte = it.getIntExtra("inte", 0)
+            if (it.hasExtra("doub"))
+                doub = it.getDoubleExtra("doub", 0.0)
+            if (it.hasExtra("flo"))
+                flo = it.getFloatExtra("flo", 0.0f)
+            if (it.hasExtra("cha"))
+                cha = it.getCharExtra("cha", 'a')
+        } ?: kotlin.run { }
 
         binding.tvString1.setText(str)
         binding.tvInt1.setText(inte.toString())
